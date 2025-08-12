cda3a018bb8d78fc7be2d6ed75524e0a1361dcd5
fish <I@fishfish.date>
2025-08-12 09:08:33 +0800

    chore(router): 添加 unplugin-vue-router 插件并配置自动路由
    - 在 vite.config.ts 中添加 VueRouter 插件
    - 在 src/router/index.ts 中使用自动生成的路由配置
    - 在 env.d.ts 中添加 unplugin-vue-router/client 引用
    - 生成并添加 typed-router.d.ts 文件

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,19 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import AutoImport from "unplugin-auto-import/vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
+import VueRouter from "unplugin-vue-router/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({
+      dts: "src/typed-router.d.ts",
+    }),
     vue(),
     vueJsx(),
     vueDevTools(),
     AutoImport({
-      dts: 'src/auto-imports.d.ts',
+      dts: "src/auto-imports.d.ts",
       imports: [
         "vue",
         {
