a1724794a451fe263a66a9cf7ff8c5d3dc5ecafb
fish <I@fishfish.date>
2025-08-13 09:56:53 +0800

    style(layout): 格式化 PageSider 组件代码
    - 使用 ESLint 规则格式化了代码
    - 调整了缩进、空格和换行
    - 统一了字符串引号使用双引号

--- src/components/layout/parts/PageSider.vue
+++ src/components/layout/parts/PageSider.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import { h } from 'vue';
-import { RouterLink } from 'vue-router';
-import type { MenuOption } from 'naive-ui';
+import { h } from "vue";
+import { RouterLink } from "vue-router";
+import type { MenuOption } from "naive-ui";
 
 const menuOptions: MenuOption[] = [
   {
-    label: () => h(RouterLink, { to: { path: '/' } }, { default: () => '概览' }),
-    key: 'dashboard',
+    label: () => h(RouterLink, { to: { path: "/overview" } }, { default: () => "概览" }),
+    key: "dashboard",
   },
   {
-    label: () => h(RouterLink, { to: { path: '/supplier' } }, { default: () => '供应商管理' }),
-    key: 'supplier',
+    label: () => h(RouterLink, { to: { path: "/supplier" } }, { default: () => "供应商管理" }),
+    key: "supplier",
   },
   {
-    label: () => h(RouterLink, { to: { path: '/system' } }, { default: () => '系统设置' }),
-    key: 'system',
+    label: () => h(RouterLink, { to: { path: "/system" } }, { default: () => "系统设置" }),
+    key: "system",
   },
 ];
 </script>
@@ -28,10 +28,6 @@ const menuOptions: MenuOption[] = [
     :width="240"
     :native-scrollbar="false"
   >
-    <n-menu
-      :collapsed-width="64"
-      :collapsed-icon-size="22"
-      :options="menuOptions"
-    />
+    <n-menu :collapsed-width="64" :collapsed-icon-size="22" :options="menuOptions" />
   </n-layout-sider>
 </template>
